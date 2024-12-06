e74dff4c167abce9f7d9f6d3fa72aee7afb0b6c8
Oni <walidfikry5@gmail.com>
2024-12-07 00:02:28 +0100

    codici lezioni di oggi

--- 1-anno/programmazione/lezioni/clang/dynamic_vector.c
+++ 1-anno/programmazione/lezioni/clang/dynamic_vector.c
@@ -1,16 +1,37 @@
 #include<stdio.h>
 
-int main(){
 
-  int n = 10;
-  float +a = malloc(n*sizeof(float)); // costo temp O(1)
-
-  for(int i = 0; i < n; i++){
-    a[i] = 10+i;
-  }
-
-  a = realloc(a, (n+1)*sizeof(float)); 
-  // nel caso peggiore O(n) (se fa la copia)
-
-  a[n] = 0;
+//funzione che ritorna il tipo da una stringa buf che viene letta da tastiera
+//questo approccio ci permetterà di inserire elementi in una lista generica
+char get_type(char* buf){
+    int tmp_int;
+    float tmp_float;
+    if(sscanf(buf,"%f",&tmp_float) || sscanf(buf,"%d",&tmp_int)){ //14.012
+        int i = 0, n_p = 0;
+        while(buf[i] != '\0'){
+          if(buf[i] == '.'){
+            n_p++;
+            if(n_p > 1)
+              return 's';
+          }else if(buf[i] < '0' || buf[i] > '9')
+            return 's';
+          i++;
+        }
+        if(n_p == 1){
+          return 'f';
+        }
+      return 'i';    
+    }
+  return 's';
 }
+
+
+int main(){
+    char buf[1024];
+    scanf("%s",buf);
+    printf("%c\n",get_type(buf));
+}
+
+
+
+
